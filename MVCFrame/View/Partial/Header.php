88522725e8c24bb643dbd316631d3855c782e1fa
Zoltan Fischer <zoli_fischer@yahoo.com>
2018-08-30 18:58:04 +0200

    fixup! Added assets url with force recache on change

--- MVCFrame/View/Partial/Header.php
+++ MVCFrame/View/Partial/Header.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/css/vendor/font-awesome/css/fontawesome-all.css'));?>" />
     <link rel="stylesheet" href="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/css/vendor/bootstrap/bootstrap.min.css'));?>" />
     <link rel="stylesheet" href="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/css/main.css'));?>" />
-    
-    <script src="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/js/vendor/jquery/jquery.min.js?d'));?>"></script>
+
+    <script src="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/js/vendor/jquery/jquery.min.js'));?>"></script>
     <script src="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/js/vendor/bootstrap/bootstrap.bundle.min.js'));?>"></script>
     <script src="<?=MVCFrame\StringFormatter::ToHtmlSafe(MVCFrame\Url::Recache('assets/js/main.js'));?>"></script>
 </head>
